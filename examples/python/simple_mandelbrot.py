428f02f1eed212d17da5737b9d996dcf921a584f
Alberto Gonzalez <mindhells@gmail.com>
2020-05-15 12:31:59 +0100

    add custom gradient to python example

--- examples/python/simple_mandelbrot.py
+++ examples/python/simple_mandelbrot.py
@@ -5,8 +5,9 @@ import sys
 project_path = os.path.dirname(os.path.abspath(__file__)) + '/../..'
 sys.path.insert(0, project_path)
 
+import shutil
 from fract4d_compiler import fc
-from fract4d import fractconfig, fract4dc, image
+from fract4d import fractconfig, fract4dc, image, gradient
 from fract4d.tests.fractalsite import FractalSite
 
 # 1st get the userConfig (containing compiler flags based on the platform among other things)
@@ -18,13 +19,22 @@ compiler = fc.Compiler(userConfig)
 # 3rd compile the formula with the inside and outside color transfer algorithms
 formula = compiler.get_formula("gf4d.frm", "Mandelbrot")
 inside_transfer = compiler.get_formula("gf4d.cfrm", "zero", "cf1")
-outside_transfer = compiler.get_formula("gf4d.cfrm", "continuous_potential", "cf0")
+outside_transfer = compiler.get_formula("standard.ucl", "OrbitTraps", "cf0")
+# outside_transfer = compiler.get_formula("gf4d.cfrm", "default", "cf0")
 compiled_library = compiler.compile_all(formula, outside_transfer, inside_transfer, [])
+
+(base, _) = os.path.splitext(compiled_library)
+shutil.copy(base + ".c", "examples/output/simple_mandelbrot.c")
 
 # 4th get the formula and transfer algorithms params in a single list
 formula_and_transfer_params = formula.symbols.default_params() + \
                             outside_transfer.symbols.default_params() + \
                             inside_transfer.symbols.default_params()
+
+print("formula params:", formula.symbols.param_names())
+print("outside_transfer params:", outside_transfer.symbols.param_names())
+print("inside_transfer params:", inside_transfer.symbols.param_names())
+print("param values", formula_and_transfer_params)
 
 # 5th stablish the location params aka the perspective form which you view the fractal
 location_params = [
@@ -41,13 +51,15 @@ message_handler = FractalSite()
 controller.set_message_handler(message_handler)
 
 # 8th create a color map to convert point fates (iterations needed to determine if the point scapes) into colors
-color_map = fract4dc.cmap_create(
-    [
-        (0.0, 0, 0, 0, 255),
-        (1 / 256.0, 255, 255, 255, 255),
-        (1.0, 255, 255, 255, 255)
-    ]
-)
+gradient = compiler.get_gradient("Autumn.cs", None)
+# color_map = fract4dc.cmap_create(
+#     [
+#         (0.0, 0, 0, 0, 255),
+#         (1 / 256.0, 255, 255, 255, 255),
+#         (1.0, 255, 255, 255, 255)
+#     ]
+# )
+color_map = fract4dc.cmap_create_gradient(gradient.segments)
 
 # 9th create an image: this one will hold the final result and provide utilities to save it into a file
 im = image.T(640, 480)
